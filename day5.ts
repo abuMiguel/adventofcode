41d59243792c00359e04780709b659a512d56f00
abuMiguel <joseph.petersen2@gmail.com>
2021-12-05 11:01:20 -0500

    day 5 part 2

--- day5.ts
+++ day5.ts
@@ -18,10 +18,11 @@ const points = input.map(i => new Points(i));
 const graph = new Set<string>();
 const intersected = new Set<string>();
 
-function addAndCheckIntersection(val: string){
+function addAndCheckIntersection(val: string) {
     !graph.has(val) ? graph.add(val) : !intersected.has(val) ? intersected.add(val) : null;
 }
 
+//Part 1
 points.forEach(p => {
     // Vertical line
     if (p.x1 === p.x2) {
@@ -34,7 +35,7 @@ points.forEach(p => {
     }
 
     // Horizontal line
-    else if (p.y1 === p.y2) {
+    if (p.y1 === p.y2) {
         const largerX = Math.max(p.x1, p.x2);
         const smallerX = Math.min(p.x1, p.x2);
         for (let i = smallerX; i <= largerX; i++) {
@@ -42,22 +43,26 @@ points.forEach(p => {
             addAndCheckIntersection(val);
         }
     }
+});
+console.log(intersected.size);
 
-    // Part 2
-    // Diagon Alley
-    else {
-        const distX = p.x1 - p.x2;
-        const isXIncrementing = distX < 0;
-        const distY = p.y1 - p.y2;
-        const isYIncrementing = distY < 0;
+// Part 2
+// Diagon Alley
+graph.clear();
+points.forEach(p => {
+    const distX = p.x1 - p.x2;
+    const isXIncrementing = distX < 0;
+    const distY = p.y1 - p.y2;
+    const isYIncrementing = distY < 0;
+    const isVertical = p.x1 === p.x2;
+    const isHorizontal = p.y1 === p.y2;
 
-        const loopLength = Math.abs(distX);
-        for(let i = 0; i <= loopLength; i++){
-            const xVal = isXIncrementing ? `${p.x1 + i}` : `${p.x1 - i}`;
-            const yVal = isYIncrementing ? `${p.y1 + i}` : `${p.y1 - i}`;
-            const val = `${xVal},${yVal}`;
-            addAndCheckIntersection(val);
-        }
+    const loopLength = distX === 0 ? Math.abs(distY) : Math.abs(distX);
+    for (let i = 0; i <= loopLength; i++) {
+        const xVal = isXIncrementing ? `${p.x1 + i}` : isVertical ? `${p.x1}` : `${p.x1 - i}`;
+        const yVal = isYIncrementing ? `${p.y1 + i}` : isHorizontal ? `${p.y1}` : `${p.y1 - i}`;
+        const val = `${xVal},${yVal}`;
+        addAndCheckIntersection(val);
     }
 });
 
