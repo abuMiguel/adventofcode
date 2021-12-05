cf42950195d880df5e1f23149576a3e6de8413e0
abuMiguel <joseph.petersen2@gmail.com>
2021-12-05 02:40:34 -0500

    day5 part 1

---
+++ day5.ts
@@ -0,0 +1,47 @@
+import { day5Input } from "./day5.data";
+
+const input = day5Input.split("\n");
+
+class Points {
+    constructor(input: string) {
+        const nums = input.split("->").map(v => v.trim());
+        [this.x1, this.y1] = nums[0].split(",").map(x => +x);
+        [this.x2, this.y2] = nums[1].split(",").map(y => +y);
+    }
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+}
+
+const points = input.map(i => new Points(i));
+const graph = new Set<string>();
+const intersected = new Set<string>();
+
+function addAndCheckIntersection(val: string){
+    !graph.has(val) ? graph.add(val) : !intersected.has(val) ? intersected.add(val) : null;
+}
+
+points.forEach(p => {
+    // Vertical line
+    if (p.x1 === p.x2) {
+        const largerY = Math.max(p.y1, p.y2);
+        const smallerY = Math.min(p.y1, p.y2);
+        for (let i = smallerY; i <= largerY; i++) {
+            const val = `${p.x1},${i}`;
+            addAndCheckIntersection(val);
+        }
+    }
+
+    // Horizontal line
+    if (p.y1 === p.y2) {
+        const largerX = Math.max(p.x1, p.x2);
+        const smallerX = Math.min(p.x1, p.x2);
+        for (let i = smallerX; i <= largerX; i++) {
+            const val = `${i},${p.y1}`;
+            addAndCheckIntersection(val);
+        }
+    }
+});
+
+console.log(intersected.size);
