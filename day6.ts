f57ff03c642ede92cf0689f91516bfa286505d12
abuMiguel <joseph.petersen2@gmail.com>
2021-12-08 18:39:20 -0500

    day 6 part 1

--- day6.ts
+++ day6.ts
@@ -1,7 +1,16 @@
 import { day6Input } from './data/day6.data';
-import { from, map, take } from 'rxjs';
+import { take, of, expand, last, tap } from 'rxjs';
 
-const fish = day6Input.split(",").map(f => +f);
-const fish$ = from(fish);
+console.time("day6");
 
+const fishInput = day6Input.split(",").map(f => +f);
+const numDays = 80;
 
+const fishes$ = of(fishInput).pipe(
+    expand(fish => of(fish.flatMap(x => x === 0 ? [6, 8] : x-1))),
+    take(numDays + 1)
+);
+
+fishes$.pipe(last(), tap(n => console.log(n.length))).subscribe({
+    complete: () => console.timeEnd("day6")
+});
