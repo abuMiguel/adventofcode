43a87974ed83c39fb0264e35060fa972b1f746c0
abuMiguel <joseph.petersen2@gmail.com>
2021-12-09 09:51:11 -0500

    add type

--- day6.ts
+++ day6.ts
@@ -1,12 +1,12 @@
 import { day6Input } from './data/day6.data';
-import { take, of, expand, last, tap } from 'rxjs';
+import { take, of, expand, last, tap, Observable } from 'rxjs';
 
 console.time("day6");
 
 const fishInput = day6Input.split(",").map(f => +f);
 const numDays = 80;
 
-const fishes$ = of(fishInput).pipe(
+const fishes$: Observable<number[]> = of(fishInput).pipe(
     expand(fish => of(fish.flatMap(x => x === 0 ? [6, 8] : x-1))),
     take(numDays + 1)
 );
